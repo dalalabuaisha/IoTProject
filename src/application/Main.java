72b82bc8d5c0df4a48fbc3fca58b5903eeb80e1d
dalalabuaisha <DalalAbuaisha@dalals-mbp>
2017-11-28 01:20:49 +0000

    Uploading the interface tool

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,19 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Pane root = new Pane();
+			root = FXMLLoader.load(getClass().getResource("Tool.fxml"));
+			Scene scene = new Scene(root,300,360);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -23,4 +25,5 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
 }
